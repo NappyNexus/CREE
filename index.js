a8976c1e272bb3d4435f1ecc254517d3d624cc6c
NappyNexus <gabrielsoriano172@gmail.com>
2025-05-21 01:28:44 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const mobileMenuButtton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-const openMenu = mobileMenuButtton.querySelector('svg:first-child');
-const closeMenu = mobileMenuButtton.querySelector('svg:last-child');
+// const mobileMenuButtton = document.getElementById('mobile-menu-button');
+// const mobileMenu = document.getElementById('mobile-menu');
+// const openMenu = mobileMenuButtton.querySelector('svg:first-child');
+// const closeMenu = mobileMenuButtton.querySelector('svg:last-child');
 
-mobileMenuButtton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-    openMenu.classList.toggle('hidden');
-    closeMenu.classList.toggle('hidden');
-})
+// mobileMenuButtton.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('hidden');
+//     openMenu.classList.toggle('hidden');
+//     closeMenu.classList.toggle('hidden');
+// })
